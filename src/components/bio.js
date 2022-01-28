a3d45515abbd6cc7351f459f1e71ffac164bab45
Claus Guttesen <kometen@gmail.com>
2022-01-29 00:22:08 +0100

    Preload css to prevent icons from showing on pageload.

--- src/components/bio.js
+++ src/components/bio.js
@@ -10,6 +10,9 @@ import {  faStackOverflow, faGithub, faTwitter } from '@fortawesome/free-brands-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
